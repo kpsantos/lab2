0e86553ad9a2b189e20589e9b4c2e68d97c11805
Kyle Santos <kpsantos@student.apc.edu.ph>
2024-01-15 16:42:59 +0800

    Lesson 1 - JS Introduction

---
+++ week5/scripting.js
@@ -0,0 +1,11 @@
+//loading in javascript
+let link = document.createElement('link');
+//attribute setting
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = 'website.css';
+
+//html head element append link element
+function button1(){
+    document.getElementById("buttonid1").innerHTML = "Hello javascript!"
+}
