a1cd61c3bfe893511ec43e389035a2ec3a683ab5
Kyle Santos <kpsantos@student.apc.edu.ph>
2024-02-08 21:18:46 +0800

    back to normal

--- week8/phpScript.php
+++ week8/phpScript.php
@@ -131,31 +131,31 @@
                     echo $gender;
                     ?>
                     <?php
-                      $servername = "localhost";
-                      $username = "webprogmi222_sf221";
-                      $password = "xE*Y2nleNVvZm[!!";
-                      $dbname = "webprogmi222_sf221";
+                    $servername = "localhost";
+                    $username = "webprogmi222_sf221";
+                    $password = "xE*Y2nleNVvZm[!!";
+                    $dbname = "webprogmi222_sf221";
 
-                      // Create connection
-                      $conn = new mysqli($servername, $username, $password, $dbname);
-                      // Check connection
-                      if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                      }
+                    // Create connection
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    // Check connection
+                    if ($conn->connect_error) {
+                      die("Connection failed: " . $conn->connect_error);
+                    }
 
-                      $sql = "SELECT * FROM kpsantos_myguests";
-                      $result = $conn -> query($sql);
+                    $sql = "SELECT * FROM kpsantos_myguests";
+                    $result = $conn -> query($sql);
 
-                      if ($result->num_rows > 0){
-                          //output data each row
-                          while($row = $result -> fetch_assoc()) {
-                              echo . $row["name"]."\n". $row["email"]. "\n". $row["website"]. " \n". $row["comment"]. "\n". $row["gender"]. "<br>";
-                          }
-                      }   else {
-                          echo "0 results";
-                      }
-                      $conn->close();
-                      ?>
+                    if ($result->num_rows > 0){
+                        //output data each row
+                        while($row = $result -> fetch_assoc()) {
+                            echo "id: ". $row["id"]. " - Name: ". $row["name"]. " - Email: ". $row["email"]. " - Website: ". $row["website"]. " - Comment: ". $row["comment"]. " - Gender: ". $row["gender"]. "<br>";
+                        }
+                    }   else {
+                        echo "0 results";
+                    }
+                    $conn->close();
+                    ?>
         </section>
 
         
