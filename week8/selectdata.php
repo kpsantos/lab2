370142abaf0a973422eaa9199885b71e6aae200c
Kyle Santos <kpsantos@student.apc.edu.ph>
2024-02-08 20:43:28 +0800

    back to zero

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -1,93 +1,26 @@
-<!DOCTYPE html>
-<html>
+<?php
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "myDB";
 
-<link rel="stylesheet" href="website.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<!--open sans font-->
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&family=Roboto&display=swap" rel="stylesheet">
-<!--logo font-->
-<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+ // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+ if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+ }
 
+ $sql = "SELECT id, name, email, website, comment, gender FROM myguests";
+ $result = $conn -> query($sql);
 
-
-<head>
-	<title>shortscouture!</title>
-	<link rel="icon" type="image/xicon" href="images/chitoge.ico">
-</head>
-
-<body class = "bg">
-
- 
-  <!--navbar-->
-  <nav class ="basta">
-    <a class="logo">shortscouture</a>
-      <div class="navbar">
-          <a href="index.php">Home</a>
-          <a onclick="darkMode()">Test</a>
-          <a href="#">Misc</a>
-      </div>
-  </nav>
-  <!--html structure-->
-  <main>
-    <div class="welcome-container">
-    <header class="welcome-text">PHP Entry</header>
-    </div>
-    <div class="article">
-      <div class="aboutme">
-        <section class="aboutme1">
-        <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "myDB";
-
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection
-            if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-            }
-
-            $sql = "SELECT id, name, email, website, comment, gender FROM myguests";
-            $result = $conn -> query($sql);
-
-            if ($result->num_rows > 0){
-                //output data each row
-                while($row = $result -> fetch_assoc()) {
-                    echo "id: ". $row["id"]. " - Name: ". $row["name"]. " - Email: ". $row["email"]. " - Website: ". $row["website"]. " - Comment: ". $row["comment"]. " - Gender: ". $row["gender"]. "<br>";
-                }
-            }   else {
-                echo "0 results";
-            }
-            $conn->close();
+if ($result->num_rows > 0){
+    //output data each row
+    while($row = $result -> fetch_assoc()) {
+        echo "id: ". $row["id"]. " - Name: ". $row["name"]. " - Email: ". $row["email"]. " - Website: ". $row["website"]. " - Comment: ". $row["comment"]. " - Gender: ". $row["gender"]. "<br>";
+    }
+}   else {
+    echo "0 results";
+}
+ $conn->close();
 ?>
-
-        </section>
-
-        
-    <div class = "flexboxlink">
-      <p class="imagecontainer">
-        <a href="https://www.youtube.com/channel/UCH2a-iczGPi3FUlCG3pVD4A">
-          <img src="images/youtube.svg">
-        </a> 
-      </p>
-      <p class = "imagecontainer">
-        <a href="https://www.linkedin.com/in/kyle-santos-5550b3249/">
-          <img src="images/linkedin.svg">
-        </a>
-      </p>
-      <p class="imagecontainer">
-        <a href="https://www.last.fm/user/chanji1"><img src="images/lastfm.svg">
-        </a>
-      </p>
-      <p class="imagecontainer">
-        <a href="https://open.spotify.com/user/9npvet1shp7a8u0ekq5zzl30g"><img src="images/spotify.svg">
-        </a>
-      </p>
-    </div>
-
-    <script src="scripting.js"></script>
-</body>
-
-</html>
