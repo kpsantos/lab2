a4dba5118a36422eeca8704bd72399c386016253
Kyle Santos <kpsantos@student.apc.edu.ph>
2024-02-08 21:12:00 +0800

    Added selectdata php to phpscript

--- week8/phpScript.php
+++ week8/phpScript.php
@@ -131,29 +131,31 @@
                     echo $gender;
                     ?>
                     <?php
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "";
-                    $dbname = "myDB";
+                      $servername = "localhost";
+                      $username = "webprogmi222_sf221";
+                      $password = "xE*Y2nleNVvZm[!!";
+                      $dbname = "webprogmi222_sf221";
 
-                    // Create connection
-                    $conn = new mysqli($servername, $username, $password, $dbname);
-                    // Check connection
-                    if ($conn->connect_error) {
-                      die("Connection failed: " . $conn->connect_error);
-                    }
+                      // Create connection
+                      $conn = new mysqli($servername, $username, $password, $dbname);
+                      // Check connection
+                      if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                      }
 
-                    $sql = "INSERT INTO MyGuests (name, email, website, comment, gender)
-                    VALUES ('$name','$email','$website','$comment','$gender')";
+                      $sql = "SELECT * FROM kpsantos_myguests";
+                      $result = $conn -> query($sql);
 
-                    if ($conn->query($sql) === TRUE) {
-                      echo "New record created successfully";
-                    } else {
-                      echo "Error: " . $sql . "<br>" . $conn->error;
-                    }
-
-                    $conn->close();
-                    ?>
+                      if ($result->num_rows > 0){
+                          //output data each row
+                          while($row = $result -> fetch_assoc()) {
+                              echo "id: ". $row["id"]. " - Name: ". $row["name"]. " - Email: ". $row["email"]. " - Website: ". $row["website"]. " - Comment: ". $row["comment"]. " - Gender: ". $row["gender"]. "<br>";
+                          }
+                      }   else {
+                          echo "0 results";
+                      }
+                      $conn->close();
+                      ?>
         </section>
 
         
