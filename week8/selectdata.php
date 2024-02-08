9855cc9020c497c8ee447e4ea5f5aef4d6416111
Kyle Santos <kpsantos@student.apc.edu.ph>
2024-02-08 21:01:49 +0800

    Added website features to check if it works

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -1,4 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<link rel="stylesheet" href="website.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<!--open sans font-->
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&family=Roboto&display=swap" rel="stylesheet">
+<!--logo font-->
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+
+
+
+<head>
+	<title>shortscouture!</title>
+	<link rel="icon" type="image/xicon" href="images/chitoge.ico">
+</head>
+
+<body class = "bg">
 <?php
+?>
+
+ 
+  <!--navbar-->
+  <nav class ="basta">
+    <a class="logo">shortscouture</a>
+      <div class="navbar">
+          <a href="index.php">Home</a>
+          <a onclick="darkMode()">Test</a>
+          <a href="#">Misc</a>
+      </div>
+  </nav>
+  <!--html structure-->
+  <main>
+    <div class="welcome-container">
+    <header class="welcome-text">PHP Entry</header>
+    </div>
+    <div class="article">
+      <div class="aboutme">
+        <section class="aboutme1">
+        <?php
 $servername = "localhost";
 $username = "webprogmi222_sf221";
 $password = "xE*Y2nleNVvZm[!!";
@@ -24,3 +64,31 @@ if ($result->num_rows > 0){
 }
  $conn->close();
 ?>
+        </section>
+
+        
+    <div class = "flexboxlink">
+      <p class="imagecontainer">
+        <a href="https://www.youtube.com/channel/UCH2a-iczGPi3FUlCG3pVD4A">
+          <img src="images/youtube.svg">
+        </a> 
+      </p>
+      <p class = "imagecontainer">
+        <a href="https://www.linkedin.com/in/kyle-santos-5550b3249/">
+          <img src="images/linkedin.svg">
+        </a>
+      </p>
+      <p class="imagecontainer">
+        <a href="https://www.last.fm/user/chanji1"><img src="images/lastfm.svg">
+        </a>
+      </p>
+      <p class="imagecontainer">
+        <a href="https://open.spotify.com/user/9npvet1shp7a8u0ekq5zzl30g"><img src="images/spotify.svg">
+        </a>
+      </p>
+    </div>
+
+    <script src="scripting.js"></script>
+</body>
+
+</html>
